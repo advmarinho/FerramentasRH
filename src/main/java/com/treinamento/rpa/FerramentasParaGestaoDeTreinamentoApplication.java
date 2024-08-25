8ca25a380c3846d13dd3b325880c59461c2095be
Anderson Marinho <marinho.1983@gmail.com>
2024-08-25 16:08:50 -0300

    Projeto Treinamentos

---
+++ src/main/java/com/treinamento/rpa/FerramentasParaGestaoDeTreinamentoApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.rpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentasParaGestaoDeTreinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentasParaGestaoDeTreinamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento/rpa/FerramentasParaGestaoDeTreinamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.rpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentasParaGestaoDeTreinamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
